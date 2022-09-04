016e08a0180a22e2def848eef99186a6a75049cb
Jai-10 <jai.vaidya18@gmail.com>
2022-09-04 15:56:26 +0530

    Testing the deployment

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         
-        <p>P tag in App.jsx</p>
+        <p>testing the react app</p>
         
       </div>
     );
